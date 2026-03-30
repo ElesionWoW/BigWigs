6ee8bdc6f428e3c556c53e342619ebef3e8296e5
pepopo978 <pepopo978@gmail.com>
2026-03-30 13:43:12 -0700

    fix ancientcorehound localization

--- Raids/MC/AncientCoreHound.lua
+++ Raids/MC/AncientCoreHound.lua
@@ -13,7 +13,7 @@ L:RegisterTranslations("enUS", function() return {
 	bars_desc = "Toggles showing bars for timers.",
 	
 	
-	trigger_debuff = "afflicted by (.+).", --CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE // CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE // CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE
+	trigger_debuff = "afflicted by (.+) ", --CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE // CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE // CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE
 	trigger_debuffFail = "Ancient Core Hound's (.+) fails.", --CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE // CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE // CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE
 	trigger_debuffResist = "Ancient Core Hound's (.+) was resisted by", --CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE // CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE
 	bar_debuff = "Debuff",
@@ -29,18 +29,25 @@ L:RegisterTranslations("enUS", function() return {
 	msg_witheringHeat = "Withering Heat - Dispel!",
 	
 	msg_ancientHysteria = "Ancient Hysteria - Decurse!",
+
+	s_ancientdread = "Ancient Dread",
+	s_ancientdespair = "Ancient Despair",
+	s_groundstomp = "Ground Stomp",
+	s_cauterizingflames = "Cauterizing Flames",
+	s_witheringheat = "Withering Heat",
+	s_ancienthysteria = "Ancient Hysteria",
 } end )
 
 local timer = {
 	debuffFirst = 12,
 	debuff = 14,
-	
+
 	ancientDespair = 5,
 	groundStomp = 5,
 }
 local icon = {
 	debuff = "Spell_Shadow_UnholyFrenzy",
-	
+
 	ancientDread = "spell_shadow_curseoftounges",
 	ancientDespair = "spell_shadow_mindsteal",
 	groundStomp = "ability_warstomp",
@@ -59,11 +66,11 @@ local syncName = {
 
 function module:OnEnable()
 	--self:RegisterEvent("CHAT_MSG_SAY", "Event") --Debug
-	
+
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event") --trigger_debuff
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Event") --trigger_debuff
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Event") --trigger_debuff
-	
+
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "Event") --trigger_debuffFail
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "Event") --trigger_debuffFail, trigger_debuffResist
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE", "Event") --trigger_debuffFail, trigger_debuffResist
@@ -86,19 +93,29 @@ function module:OnDisengage()
 
 end
 
+local function IsTrackedDebuff(name)
+  print(name)
+	return name == L["s_ancientdread"]
+		or name == L["s_ancientdespair"]
+		or name == L["s_groundstomp"]
+		or name == L["s_cauterizingflames"]
+		or name == L["s_witheringheat"]
+		or name == L["s_ancienthysteria"]
+end
+
 function module:Event(msg)
 	if string.find(msg, L["trigger_debuff"]) then
 		local _,_, debuff, _ = string.find(msg, L["trigger_debuff"])
 		if debuff == "Ancient Dread" or "Ancient Despair" or "Ground Stomp" or "Cauterizing Flames" or "Withering Heat" or "Ancient Hysteria" then
 			self:Sync(syncName.debuff .. " " .. debuff)
 		end
-	
+
 	elseif string.find(msg, L["trigger_debuffFail"]) then
 		local _,_, debuff, _ = string.find(msg, L["trigger_debuffFail"])
 		if debuff == "Ancient Dread" or "Ancient Despair" or "Ground Stomp" or "Cauterizing Flames" or "Withering Heat" or "Ancient Hysteria" then
 			self:Sync(syncName.debuff .. " " .. debuff)
 		end
-	
+
 	elseif string.find(msg, L["trigger_debuffResist"]) then
 		local _,_, debuff, _ = string.find(msg, L["trigger_debuffResist"])
 		if debuff == "Ancient Dread" or "Ancient Despair" or "Ground Stomp" or "Cauterizing Flames" or "Withering Heat" or "Ancient Hysteria" then
@@ -116,43 +133,45 @@ end
 
 
 function module:Debuff(rest)
+    print(rest)
+
 	self:RemoveBar(L["bar_debuff"])
-	
-	if rest == "Ancient Dread" then
+
+	if rest == L["s_ancientdread"] then
 		self:Bar(L["bar_debuff"], timer.debuff, icon.debuff, true, color.debuff)
 		if UnitClass("Player") == "Priest" or UnitClass("Player") == "Paladin" then
 			self:Message(L["msg_ancientDread"], "Urgent", false, nil, false)
 			self:Sound("Info")
 			self:WarningSign(icon.ancientDread, 1)
 		end
-	
-	elseif rest == "Ancient Despair" then
+
+	elseif rest == L["s_ancientdespair"] then
 		self:Bar(L["bar_ancientDespair"], timer.ancientDespair, icon.ancientDespair, true, color.ancientDespair)
 		self:WarningSign(icon.ancientDespair, 1)
 		self:DelayedBar(timer.ancientDespair, L["bar_debuff"], timer.debuff - timer.ancientDespair, icon.debuff, true, color.debuff)
-	
-	elseif rest == "Ground Stomp" then
+
+	elseif rest == L["s_groundstomp"] then
 		self:Bar(L["bar_groundStomp"], timer.groundStomp, icon.groundStomp, true, color.groundStomp)
 		self:WarningSign(icon.groundStomp, 1)
 		self:DelayedBar(timer.groundStomp, L["bar_debuff"], timer.debuff - timer.groundStomp, icon.debuff, true, color.debuff)
-	
-	elseif rest == "Cauterizing Flames" then
+
+	elseif rest == L["s_cauterizingflames"] then
 		self:Bar(L["bar_debuff"], timer.debuff, icon.debuff, true, color.debuff)
 		if UnitClass("Player") == "Priest" or UnitClass("Player") == "Paladin" then
 			self:Message(L["msg_cauterizingFlames"], "Urgent", false, nil, false)
 			self:Sound("Info")
 			self:WarningSign(icon.cauterizingFlames, 1)
 		end
-		
-	elseif rest == "Withering Heat" then
+
+	elseif rest == L["s_witheringheat"] then
 		self:Bar(L["bar_debuff"], timer.debuff, icon.debuff, true, color.debuff)
 		if UnitClass("Player") == "Priest" or UnitClass("Player") == "Paladin" then
 			self:Message(L["msg_witheringHeat"], "Urgent", false, nil, false)
 			self:Sound("Info")
 			self:WarningSign(icon.witheringHeat, 1)
 		end
-		
-	elseif rest == "Ancient Hysteria" then
+
+	elseif rest == L["s_ancienthysteria"] then
 		self:Bar(L["bar_debuff"], timer.debuff, icon.debuff, true, color.debuff)
 		if UnitClass("Player") == "Mage" or UnitClass("Player") == "Druid" then
 			self:Message(L["msg_ancientHysteria"], "Urgent", false, nil, false)

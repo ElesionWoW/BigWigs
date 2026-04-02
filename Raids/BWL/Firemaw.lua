0c01434f3eabc109b7ce54025208e4ca60660371
ElesionWoW <postsack10@gatebase.com>
2026-04-02 16:04:37 +0200

    Update Firemaw.lua
    * Warn when unsoaked floor zones explode
    * Optionally show timers for soak duration (8s = 5 casts with 2s in between).
    * Fixed Shadow Flame tigger: "begins to cast Shadow Flame" no longer appears in logs; rebased important timers on "Shadow Flame hits" and added a new way to fetch the cast event through SuperWoW (1 person with SuperWoW can sync it to the raid)

--- Raids/BWL/Firemaw.lua
+++ Raids/BWL/Firemaw.lua
@@ -3,7 +3,18 @@ local module, L = BigWigs:ModuleDeclaration("Firemaw", "Blackwing Lair")
 
 module.revision = 30085
 module.enabletrigger = module.translatedName
-module.toggleoptions = {"wingbuffet", "shadowflame", "flamebuffet", "stacks", "bosskill"}
+module.toggleoptions = {"wingbuffet", "shadowflame", "sfzonealert", "sfzonebar", "sfexplosion", "flamebuffet", "stacks", "bosskill"}
+
+-- module defaults
+module.defaultDB = {
+	wingbuffet = true,
+	shadowflame = true,
+	sfzonealert = true,
+	sfzonebar = false,
+	sfexplosion = true,
+	flamebuffet = false,
+	stacks = true,
+}
 
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Firemaw",
@@ -14,7 +25,19 @@ L:RegisterTranslations("enUS", function() return {
 
 	shadowflame_cmd = "shadowflame",
 	shadowflame_name = "Shadow Flame Alert",
-	shadowflame_desc = "Warn for Shadow Flame",
+	shadowflame_desc = "Warn for Shadow Flame casts (requires someone to have SuperWoW) and Shadow Flame CD",
+
+	sfzonealert_cmd = "sfzonealert",
+	sfzonealert_name = "Shadowflame Zone Alert",
+	sfzonealert_desc = "Warns when new floor zones (Shadowflame Jet) spawn",
+
+	sfzonebar_cmd = "sfzonebar",
+	sfzonebar_name = "Shadowflame Zone Bar",
+	sfzonebar_desc = "Shows timer bars for floor zones (Shadowflame Jet)",
+
+	sfexplosion_cmd = "sfexplosion",
+	sfexplosion_name = "Shadowflame Explosion Alert",
+	sfexplosion_desc = "Warns when Shadowflame Explosions happen (unsoaked floor zones)",
 	
 	flamebuffet_cmd = "flamebuffet",
 	flamebuffet_name = "Flame Buffet Alert",
@@ -31,10 +54,17 @@ L:RegisterTranslations("enUS", function() return {
 	msg_wingBuffetCast = "Casting Wing Buffet!",
 	msg_wingBuffetSoon = "Wing Buffet in 2 seconds - Taunt now!",
 	
-	trigger_shadowFlame = "Firemaw begins to cast Shadow Flame.", --CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE
+	trigger_shadowFlameCast = "Firemaw begins to cast Shadow Flame.", --CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE - seems to no longer appear in logs since 1.18
 	bar_shadowFlameCast = "Casting Shadow Flame!",
-	bar_shadowFlameCd = "Shadow Flame CD",
 	msg_shadowFlameCast = "Casting Shadow Flame!",
+	trigger_shadowFlameHit = "Firemaw's Shadow Flame hits", --CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE
+	bar_shadowFlameCd = "Shadow Flame CD",
+	
+	msg_sfzone = "Look for new Floor Zones!",
+	bar_sfzoneFind = "find new Zone(s)",
+	bar_sfzone = "soak Floor Zone(s)",
+	trigger_sfexplosion = "Shadowflame Jet's Shadowflame Explosion", --CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE // etc
+	warn_sfexplosion = "Unsoaked Zone!",
 	
 	trigger_flameBuffet = "Firemaw's Flame Buffet", --CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE // CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE // CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE
 		--Firemaw's Flame Buffet fails. (.+) is immune.
@@ -58,12 +88,17 @@ local timer = {
 	shadowFlameCd = 14, --16 - 2sec cast
 	shadowFlameCast = 2,
 	
+	sfzonePre = 4,
+	sfzoneDur = 8,
+	
 	flameBuffet = {1.913,4.936}, --saw 1.913 to 4.936
 }
 local icon = {
 	wingBuffet = "INV_Misc_MonsterScales_14",
 	shadowFlame = "Spell_Fire_Incinerate",
-	flameBuffet = "Spell_Fire_Fireball"
+	flameBuffet = "Spell_Fire_Fireball",
+	sfzone = "spell_shadow_antishadow",
+	sfexplosion = "Spell_Fire_SelfDestruct",
 }
 local color = {
 	wingBuffetCd = "Cyan",
@@ -71,27 +106,38 @@ local color = {
 	
 	shadowFlameCd = "Orange",
 	shadowFlameCast = "Red",
+	sfzone = "Black",
 	
 	flameBuffet = "Black"
 }
 local syncName = {
 	wingBuffet = "FiremawWingBuffet"..module.revision,
-	shadowFlame = "FiremawShadowflame"..module.revision,
+	shadowFlameCast = "FiremawShadowflame"..module.revision,
+	shadowFlameHit = "FiremawShadowflameHit"..module.revision,
+	sfexplosion = "FiremawShadowflameExplosion"..module.revision,
 	flameBuffet = "FiremawFlameBuffet"..module.revision,
+}
+local spellId = {
+	shadowFlame = 22539,
 }
 
 function module:OnEnable()
 	--self:RegisterEvent("CHAT_MSG_SAY", "Event") --Debug
 	
-	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "Event") --trigger_flameBuffet
-	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "Event") --trigger_flameBuffet
-	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE", "Event") --trigger_wingBuffet, trigger_shadowFlame, trigger_flameBuffet
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "Event") --trigger_flameBuffet, trigger_sfexplosion, trigger_shadowFlameHit
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "Event") --trigger_flameBuffet, trigger_sfexplosion, trigger_shadowFlameHit
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE", "Event") --trigger_wingBuffet, trigger_shadowFlameCast, trigger_flameBuffet, trigger_sfexplosion, trigger_shadowFlameHit
 	
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event") --trigger_flameBuffetYou
-
+	
+	if SUPERWOW_VERSION or SetAutoloot then
+		self:RegisterEvent("UNIT_CASTEVENT")
+	end
 	
 	self:ThrottleSync(3, syncName.wingBuffet)
-	self:ThrottleSync(3, syncName.shadowFlame)
+	self:ThrottleSync(3, syncName.shadowFlameCast)
+	self:ThrottleSync(3, syncName.shadowFlameHit)
+	self:ThrottleSync(1.5, syncName.sfexplosion)
 	self:ThrottleSync(1.5, syncName.flameBuffet)
 end
 
@@ -121,8 +167,14 @@ function module:Event(msg)
 	if msg == L["trigger_wingBuffet"] then
 		self:Sync(syncName.wingBuffet)
 	
-	elseif msg == L["trigger_shadowFlame"] then
-		self:Sync(syncName.shadowFlame)
+	elseif msg == L["trigger_shadowFlameCast"] then
+		self:Sync(syncName.shadowFlameCast)
+	
+	elseif string.find(msg, L["trigger_shadowFlameHit"]) then
+		self:Sync(syncName.shadowFlameHit)
+	
+	elseif string.find(msg, L["trigger_sfexplosion"]) then
+		self:Sync(syncName.sfexplosion)
 	
 	elseif string.find(msg, L["trigger_flameBuffet"]) then
 		self:Sync(syncName.flameBuffet)
@@ -136,12 +188,26 @@ function module:Event(msg)
 	end
 end
 
+function module:UNIT_CASTEVENT(caster,target,action,spell,castTime)
+	if spell == spellId.shadowFlame and action == "START" then
+		self:Sync(syncName.shadowFlameCast)
+		self:DelayedSync(castTime/1000, syncName.shadowFlameHit)
+		return
+	end
+end
 
 function module:BigWigs_RecvSync(sync, rest, nick)
 	if sync == syncName.wingBuffet and self.db.profile.wingbuffet then
 		self:WingBuffet()
-	elseif sync == syncName.shadowFlame and self.db.profile.shadowflame then
-		self:ShadowFlame()
+	elseif sync == syncName.shadowFlameCast and self.db.profile.shadowflame then
+		self:RemoveBar(L["bar_shadowFlameCd"])
+		self:Bar(L["bar_shadowFlameCast"], timer.shadowFlameCast, icon.shadowFlame, true, color.shadowFlameCast)
+		self:Message(L["msg_shadowFlameCast"], "Urgent", false, nil, false)
+	elseif sync == syncName.shadowFlameHit then
+		self:ShadowFlameHit()
+	elseif sync == syncName.sfexplosion and self.db.profile.sfexplosion then
+		self:WarningSign(icon.sfexplosion, 1, false, L["warn_sfexplosion"])
+		self:Sound("Beware")
 	elseif sync == syncName.flameBuffet and self.db.profile.flamebuffet then
 		self:FlameBuffet()
 	end
@@ -158,13 +224,20 @@ function module:WingBuffet()
 	self:DelayedMessage(timer.wingBuffetCast + timer.wingBuffetCd - 2, L["msg_wingBuffetSoon"], "Attention", false, nil, false)
 end
 
-function module:ShadowFlame()
-	self:RemoveBar(L["bar_shadowFlameCd"])
-	
-	self:Bar(L["bar_shadowFlameCast"], timer.shadowFlameCast, icon.shadowFlame, true, color.shadowFlameCast)
-	self:Message(L["msg_shadowFlameCast"], "Urgent", false, nil, false)
-	
-	self:DelayedBar(timer.shadowFlameCast, L["bar_shadowFlameCd"], timer.shadowFlameCd, icon.shadowFlame, true, color.shadowFlameCd)
+function module:ShadowFlameHit()
+	if self.db.profile.shadowflame then
+		self:RemoveBar(L["bar_shadowFlameCd"])
+
+		self:Bar(L["bar_shadowFlameCd"], timer.shadowFlameCd, icon.shadowFlame, true, color.shadowFlameCd)
+	end
+
+	if self.db.profile.sfzonealert then
+		self:Message(L["msg_sfzone"], "Core")
+	end
+	if self.db.profile.sfzonebar then
+		self:Bar(L["bar_sfzoneFind"], timer.sfzonePre, icon.sfzone, true, color.sfzone)
+		self:DelayedBar(timer.sfzonePre, L["bar_sfzone"], timer.sfzoneDur, icon.sfzone, true, color.sfzone)
+	end
 end
 
 function module:FlameBuffet()
